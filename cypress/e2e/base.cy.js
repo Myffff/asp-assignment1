54f301e19972756d21ca45a52466bb5a36c0dcf7
Yifei Ma <634042441@qq.com>
2022-11-08 19:22:23 +0800

    basic E2E test for movies page.

--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -4,7 +4,8 @@ let movie; //
 describe("Base tests", () => {
   before(() => {
     cy.request(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=26ba5e77849587dbd7df199727859189&page=1`
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${Cypress.env(
+        "TMDB_KEY")}&page=1`
     )
       .its("body") 
       .then((response) => {
@@ -28,6 +29,37 @@ describe("Base tests", () => {
     });
 
   });
+
 });
 
+describe("Base tests for movie page", () => {
+  before(() => {
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY")}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+    )
+      .its("body") 
+      .then((response) => {
+        movies = response.results;
+      });
+  });
+  beforeEach(() => {
+    cy.visit("/movie");
+  });
 
+  describe("Discover Movies page", () => {
+    it("displays the page header and 20 movies per page", () => {
+      cy.get("span").contains("MOVIE HUB");
+      cy.get(".media").should("have.length", 20);
+    });
+
+    it("displays only movies shows in this page", () => {
+      cy.get(".media").each(() => {
+        // cy.wrap($card).find("p").contains(movies[index].title||movies[index].name);
+        cy.get("span.subTitle").contains("Movie")
+      });
+    });
+
+  });
+
+});
